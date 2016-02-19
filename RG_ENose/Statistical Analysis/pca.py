f491619a2faf3c4bff200e74bc023373f6b0c4e7
98789 <lerht@hotmail.com>
2016-02-19 13:38:21 -0500

    Added PCA

---
+++ RG_ENose/Statistical Analysis/pca.py
@@ -0,0 +1,22 @@
+from scipy.stats import f
+from sklearn.decomposition import PCA
+import numpy
+
+def pca(data, scl, n_components, plot=1):
+    """Compute PCA"""
+
+    m, n = data.shape
+    pca = PCA()
+    pca.fit(data)
+    scores = pca.transform(data)[:, :n_components]
+    loads = pca.components_[:n_components, :]
+    var100 = pca.explained_variance_ratios * 100
+    ssq = zip(range(1, m + 1), pca.explained_variance_, var100, numpy.cumsum(var100))
+    res = numpy.sum((data - numpy.dot(scores, loads)) ** 2, 1)
+    q = 0
+    tsq = (n_components * (m - 1) / (m - n_components)) * f.isf(0.05, n_components, 
+           min(300, m - n_components)) if m != n_components else 0
+    tsqs = numpy.sum(numpy.dot(scores, numpy.linalg.inv(numpy.diag(ssq ** 0.5))) ** 2,
+           1) if n_components > 1 else scores ** 2 / ssq[0, 1]
+
+    return [scores, loads, ssq, res, q, tsq, tsqs]
